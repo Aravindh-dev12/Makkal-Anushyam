ab8ecf83d8f0a6a3206eaaf5280ed3896dca4a79
Aravindhan <104446384+Aravindh-dev12@users.noreply.github.com>
2026-09-19 15:52:28 +0530

    Fix WMOS database diagnostic

--- check_wms_db.php
+++ check_wms_db.php
@@ -1,9 +1,8 @@
 <?php
 require 'config.php';
-$conn = getDbConnection();
+header('Content-Type: application/json');
+
 $res = $conn->query("SELECT COUNT(*) as cnt, MIN(recorded_at) as min_d, MAX(recorded_at) as max_d FROM weather_readings");
 $row = $res ? $res->fetch_assoc() : null;
-$WMOSRes = $conn->query("SELECT COUNT(*) as cnt, MIN(recorded_at) as min_d, MAX(recorded_at) as max_d FROM weather_readings");
-$WMOSRow = $WMOSRes ? $WMOSRes->fetch_assoc() : null;
-header('Content-Type: application/json');
-echo json_encode(['weather_readings' => $row, 'weather_readings' => $WMOSRow]);
+
+echo json_encode(['weather_readings' => $row]);
